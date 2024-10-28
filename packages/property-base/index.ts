473c49366df8f683fd4553500c93b793973b7579
driatic <fredrik.hasselgren@prototyp.se>
2024-10-28 16:17:52 +0100

    MIM-212: make schema compile

--- packages/property-base/index.ts
+++ packages/property-base/index.ts
@@ -1,18 +1,18 @@
-import Koa from 'koa';
-import Router from '@koa/router';
+import Koa from 'koa'
+import Router from '@koa/router'
 
-const app = new Koa();
-const router = new Router();
-const port = 3000;
+const app = new Koa()
+const router = new Router()
+const port = 3000
 
 router.get('/', async (ctx) => {
-    ctx.body = "hello world";
-});
+    ctx.body = "hello world"
+})
 
 app
     .use(router.routes())
-    .use(router.allowedMethods());
+    .use(router.allowedMethods())
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
+    console.log(`Example app listening at http://localhost:${port}`)
+})
