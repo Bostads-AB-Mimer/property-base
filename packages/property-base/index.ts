bf3cdbfc956b50c6be326ac61f35c3598621ed34
driatic <fredrik.hasselgren@prototyp.se>
2024-10-28 14:47:38 +0100

    MIM-212: setup boilerplate api

---
+++ packages/property-base/index.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+const app = express()
+const port = 3000
+
+app.get('/', async(req, res) => {
+    res.json("hello world")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
