17e80827aec56894e2cd1474887ffdc8a3382aba
Christian Landgren <christian@landgren.nu>
2024-11-25 12:57:41 +0100

    fix: use port from config

--- packages/property-base/src/utils/links.ts
+++ packages/property-base/src/utils/links.ts
@@ -1,32 +1,40 @@
 import { Context } from 'koa'
+import port from '../config/port'
 
-export const generateMetaLinks = (ctx: Context, path: string, params?: Record<string, string>) => {
-  const port = process.env.PORT || 5050
+// this will be moved to utilities later when we have decided on the final HATEOS structure
+export const generateMetaLinks = (
+  ctx: Context,
+  path: string,
+  params?: Record<string, string>,
+) => {
   const baseUrl = `http://localhost:${port}`
-  
+
   if (params) {
     return {
       self: {
-        href: `${path}/${params.id}`
+        href: `${path}/${params.id}`,
       },
-      ...Object.entries(params).reduce((acc, [key, value]) => {
-        if (key !== 'id') {
-          acc[key] = {
-            href: `${baseUrl}/${key}/${value}`
+      ...Object.entries(params).reduce(
+        (acc, [key, value]) => {
+          if (key !== 'id') {
+            acc[key] = {
+              href: `${baseUrl}/${key}/${value}`,
+            }
           }
-        }
-        return acc
-      }, {} as Record<string, { href: string }>)
+          return acc
+        },
+        {} as Record<string, { href: string }>,
+      ),
     }
   }
 
   return {
     self: {
-      href: `${baseUrl}${path}`
+      href: `${baseUrl}${path}`,
     },
     link: {
       href: `${baseUrl}${path}`,
-      templated: false
-    }
+      templated: false,
+    },
   }
 }
