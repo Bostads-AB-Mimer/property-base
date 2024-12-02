5fba7f61a3ffdefe76d2b969b015e04a82cc699a
driatic <fredrik.hasselgren@prototyp.se>
2024-12-02 13:47:02 +0100

    MIM-291: get all rooms belonging to residence

--- src/api.ts
+++ src/api.ts
@@ -4,6 +4,7 @@ import { routes as residencesRoutes } from './services/residences'
 import { routes as buildingsRoutes } from './services/buildings'
 import { routes as propertiesRoutes } from './services/properties'
 import { routes as staircasesRoutes } from './services/staircases'
+import { routes as roomsRoutes } from './services/rooms'
 
 const router = new KoaRouter()
 
@@ -12,5 +13,6 @@ residencesRoutes(router)
 buildingsRoutes(router)
 propertiesRoutes(router)
 staircasesRoutes(router)
+roomsRoutes(router)
 
 export default router
