26a26683eb0226ec333f8b348c5f45f8d3b6e714
driatic <fredrik.hasselgren@prototyp.se>
2024-12-05 09:20:36 +0100

    MIM-288: fix company model

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,4 @@
 import KoaRouter from '@koa/router'
-
 import { routes as componentsRoutes } from './services/components'
 import { routes as residencesRoutes } from './services/residences'
 import { routes as buildingsRoutes } from './services/buildings'
