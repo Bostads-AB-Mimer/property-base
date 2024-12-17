64f89899c1466b46e2ef30e6b1f1c821a2b9505d
driatic <fredrik.hasselgren@prototyp.se>
2024-12-17 15:26:47 +0100

    MIM-327: fix typo

--- src/tests/construction-parts.test.ts
+++ src/tests/construction-parts.test.ts
@@ -1,7 +1,7 @@
 import request from 'supertest'
 import app from '../app'
 
-describe('Staircases API', () => {
+describe('Construction parts API', () => {
   let buildingCode: string
 
   beforeAll(async () => {
