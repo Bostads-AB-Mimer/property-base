7cf260d46c5da6d2160394ffbe35ed9f3317eda4
driatic <fredrik.hasselgren@prototyp.se>
2024-12-17 16:22:41 +0100

    MIM-324: update tests

--- src/tests/components.test.ts
+++ src/tests/components.test.ts
@@ -2,36 +2,28 @@ import request from 'supertest'
 import app from '../app'
 
 describe('Components API', () => {
-  let buildingCode: string
-  let floorCode: string
-  let residenceCode: string
-  let roomCode: string
-
-  beforeAll(async () => {
-    // Get codes needed for testing
+  it('should return components', async () => {
     const buildingsResponse = await request(app.callback())
       .get('/buildings')
       .query({ propertyCode: '01901' })
-    buildingCode = buildingsResponse.body.content[0].code
+    const buildingCode = buildingsResponse.body.content[0].code
 
     const staircaseResponse = await request(app.callback())
       .get('/staircases')
       .query({ buildingCode })
-    floorCode = staircaseResponse.body.content[0].code
+    const floorCode = staircaseResponse.body.content[0].code
 
     const residencesResponse = await request(app.callback())
       .get('/residences')
       .query({ buildingCode })
     const residence = residencesResponse.body.content[0]
-    residenceCode = residence.code
+    const residenceCode = residence.code
 
     const roomsResponse = await request(app.callback())
       .get('/rooms')
       .query({ buildingCode, floorCode, residenceCode })
-    roomCode = roomsResponse.body.content[2].code
-  })
+    const roomCode = roomsResponse.body.content[2].code
 
-  it('should return components for a maintenance unit', async () => {
     const response = await request(app.callback()).get('/components').query({
       buildingCode: buildingCode,
       floorCode: floorCode,
@@ -50,9 +42,12 @@ describe('Components API', () => {
   })
 
   it('should validate maintenanceUnit parameter', async () => {
-    const response = await request(app.callback())
-      .get('/components')
-      .query({ maintenanceUnit: '' }) // Empty maintenance unit code
+    const response = await request(app.callback()).get('/components').query({
+      buildingCode: '',
+      floorCode: '',
+      residenceCode: '',
+      roomCode: '',
+    })
 
     expect(response.status).toBe(400)
     expect(response.body.errors).toBeDefined()
