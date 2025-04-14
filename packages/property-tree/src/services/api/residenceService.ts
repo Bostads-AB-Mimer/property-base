a7d5f4aceebe9e82fc6a38c909880c753cec0a9c
Henrik Persson <henrik.persson@iteam.com>
2025-04-14 17:43:19 +0200

    Get residences from core

--- packages/property-tree/src/services/api/residenceService.ts
+++ packages/property-tree/src/services/api/residenceService.ts
@@ -1,18 +1,18 @@
 import { Residence, ResidenceDetails } from '../types'
-import { GET } from './baseApi'
+import { GET } from './core/base-api'
 
 export const residenceService = {
   async getByBuildingCode(buildingCode: string): Promise<Residence[]> {
-    const { data, error } = await GET('/residences', {
+    const { data, error } = await GET('/propertyBase/residences', {
       params: { query: { buildingCode } },
     })
     if (error) throw error
     return data.content || []
   },
 
-  async getById(id: string): Promise<ResidenceDetails> {
-    const { data, error } = await GET('/residences/{id}', {
-      params: { path: { id } },
+  async getById(residenceId: string): Promise<ResidenceDetails> {
+    const { data, error } = await GET('/propertyBase/residence/{residenceId}', {
+      params: { path: { residenceId } },
     })
 
     if (error) throw error
