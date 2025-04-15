935f52eed9762a7316f663f5359fa5a9fa9fff80
henrikhenrikpersson <henrik.persson@iteam.se>
2025-04-15 08:55:44 +0200

    MIM-466: Get properties from core (#49)
    * Update generated types
    
    * Get properties from core
    
    ---------
    
    Co-authored-by: Henrik Persson <henrik.persson@iteam.com>

--- packages/property-tree/src/services/api/propertyService.ts
+++ packages/property-tree/src/services/api/propertyService.ts
@@ -1,11 +1,13 @@
 import { Company } from '../types'
-import { GET } from './baseApi'
+import { GET } from './core/base-api'
+// FIXME: There is currently no GET /propertyBase/properties/{id} in onecore-core
+import { GET as LegacyGET } from './baseApi'
 
 export const propertyService = {
   // Get all properties
 
   async getFromCompany(company: Company) {
-    const { data, error } = await GET('/properties', {
+    const { data, error } = await GET('/propertyBase/properties', {
       params: { query: { companyCode: company.code } },
     })
     if (error) throw error
@@ -13,7 +15,7 @@ export const propertyService = {
   },
 
   async getPropertyById(propertyId: string) {
-    const { data, error } = await GET(`/properties/{id}`, {
+    const { data, error } = await LegacyGET(`/properties/{id}`, {
       params: { path: { id: propertyId } },
     })
     if (error) throw error
