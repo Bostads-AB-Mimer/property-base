1abfa0d03108934578ea7898c8ea18e7bd976bdb
Andreas Lundqvist <momentiris@gmail.com>
2025-04-16 14:02:25 +0200

    base: add date and prisma cases to string trim tests

--- packages/property-base/src/tests/utils/data-conversion.test.ts
+++ packages/property-base/src/tests/utils/data-conversion.test.ts
@@ -1,3 +1,4 @@
+import { Prisma } from '@prisma/client'
 import { trimStrings } from '@src/utils/data-conversion'
 
 describe(trimStrings, () => {
@@ -22,6 +23,26 @@ describe(trimStrings, () => {
   it('trims nested array of object of strings', () => {
     const input = [{ foo: [{ baz: [{ qux: '  qux  ' }, 'ok '] }] }, '  bar  ']
     const expected = [{ foo: [{ baz: [{ qux: 'qux' }, 'ok'] }] }, 'bar']
+    expect(trimStrings(input)).toEqual(expected)
+  })
+
+  it('dates pass through', () => {
+    const date = new Date('2023-01-01')
+    const input = [{ foo: [{ baz: [{ qux: date }] }] }]
+    const expected = [{ foo: [{ baz: [{ qux: date }] }] }]
+    const result = trimStrings(input)
+
+    expect(result[0].foo[0].baz[0].qux).toBeInstanceOf(Date)
+    expect(trimStrings(input)).toEqual(expected)
+  })
+
+  it('prisma decimal pass through', () => {
+    const decimal = new Prisma.Decimal(1.23)
+    const input = [{ foo: [{ baz: [{ qux: decimal }] }] }]
+    const expected = [{ foo: [{ baz: [{ qux: decimal }] }] }]
+
+    const result = trimStrings(input)
+    expect(result[0].foo[0].baz[0].qux).toBeInstanceOf(Prisma.Decimal)
     expect(trimStrings(input)).toEqual(expected)
   })
 })
