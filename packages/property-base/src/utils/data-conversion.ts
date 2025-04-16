117bdeba4e176b8c90ebddd59cd1006217a6aab0
Andreas Lundqvist <momentiris@gmail.com>
2025-04-16 13:49:06 +0200

    base: exclude prisma decimal when trimming strings

--- packages/property-base/src/utils/data-conversion.ts
+++ packages/property-base/src/utils/data-conversion.ts
@@ -1,3 +1,5 @@
+import { Prisma } from '@prisma/client'
+
 export const toBoolean = (value: number | null | undefined): boolean => {
   return Boolean(value)
 }
@@ -12,10 +14,16 @@ export function trimStrings<T>(data: T): T {
   }
 
   if (data !== null && typeof data === 'object') {
+    if (data instanceof Date) {
+      return data
+    }
+
+    if (data instanceof Prisma.Decimal) {
+      return data
+    }
+
     return Object.fromEntries(
-      Object.entries(data).map(([key, value]) => {
-        return [key, trimStrings(value)]
-      })
+      Object.entries(data).map(([key, value]) => [key, trimStrings(value)])
     ) as T
   }
 
