0bffab6564d68d9202c6f9bf6734d6d315bc27e5
Henrik Persson <henrik.persson@iteam.com>
2025-04-23 14:58:12 +0200

    Show more work order info and sort

--- packages/property-tree/src/components/shared/ResidenceWorkOrders.tsx
+++ packages/property-tree/src/components/shared/ResidenceWorkOrders.tsx
@@ -1,10 +1,49 @@
 import { useQuery } from '@tanstack/react-query'
 import { Card } from '@/components/ui/Card'
 import { Grid } from '@/components/ui/Grid'
-import { workOrderService } from '@/services/api/core/workOrderService'
+import {
+  workOrderService,
+  WorkOrder,
+} from '@/services/api/core/workOrderService'
 
 interface ResidenceWorkOrdersProps {
   rentalId: string
+}
+
+const sortByStatus = (a: WorkOrder, b: WorkOrder) => {
+  // TODO we should include the maintenance stage sequence in WorkOrder and sort by that
+  const statusOrder = [
+    'Väntar på handläggning',
+    'Resurs tilldelad',
+    'Påbörjad',
+    'Väntar på beställda varor',
+    'Utförd',
+    'Avslutad',
+  ]
+
+  const statusA = statusOrder.indexOf(a.Status)
+  const statusB = statusOrder.indexOf(b.Status)
+
+  if (statusA < statusB) {
+    return -1
+  }
+  if (statusA > statusB) {
+    return 1
+  }
+  return 0
+}
+
+const sortyByRegistered = (a: WorkOrder, b: WorkOrder) => {
+  const dateA = new Date(a.Registered)
+  const dateB = new Date(b.Registered)
+
+  if (dateA < dateB) {
+    return -1
+  }
+  if (dateA > dateB) {
+    return 1
+  }
+  return 0
 }
 
 export function ResidenceWorkOrders({ rentalId }: ResidenceWorkOrdersProps) {
@@ -31,13 +70,19 @@ export function ResidenceWorkOrders({ rentalId }: ResidenceWorkOrdersProps) {
   return (
     <Card title="Ärenden">
       <Grid cols={2}>
-        {workOrders?.map((workOrder) => (
-          <a href={workOrder.Url} target="_blank">
-            <Card key={workOrder.Id}>
-              <p>{workOrder.Caption}</p>
-            </Card>
-          </a>
-        ))}
+        {workOrders
+          ?.sort(sortyByRegistered)
+          .sort(sortByStatus)
+          .map((workOrder) => (
+            <a key={workOrder.Id} href={workOrder.Url} target="_blank">
+              <Card>
+                <p>{workOrder.Code}</p>
+                <p className="font-bold">{workOrder.Caption}</p>
+                <p>{new Date(workOrder.Registered).toLocaleString()}</p>
+                <p>{workOrder.Status}</p>
+              </Card>
+            </a>
+          ))}
       </Grid>
     </Card>
   )
