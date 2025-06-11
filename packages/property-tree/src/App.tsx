1d04bcc4012af3bdb5a0cd5d183273518ad03d1d
henrikhenrikpersson <henrik.persson@iteam.se>
2025-06-11 15:54:21 +0200

    Set redirect uri from env variable (#73)
    Co-authored-by: Henrik Persson <henrik.persson@iteam.com>

--- packages/property-tree/src/App.tsx
+++ packages/property-tree/src/App.tsx
@@ -129,6 +129,9 @@ export default function App() {
       'https://auth-test.mimer.nu/realms/onecore-test',
     clientId: import.meta.env.VITE_KEYCLOAK_CLIENT_ID || 'onecore-test',
     apiUrl: import.meta.env.VITE_CORE_API_URL || 'http://localhost:5010',
+    redirectUri:
+      import.meta.env.VITE_KEYCLOAK_REDIRECT_URI ||
+      'http://localhost:3000/callback',
   }
 
   return (
