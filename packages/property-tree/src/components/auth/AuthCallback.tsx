c502d57ddb38cd95a681d8f10831f8f52ad067d4
Andreas Lundqvist <momentiris@gmail.com>
2025-06-24 14:24:45 +0200

    tree: revise auth callback

--- packages/property-tree/src/components/auth/AuthCallback.tsx
+++ packages/property-tree/src/components/auth/AuthCallback.tsx
@@ -1,39 +1,63 @@
 import React from 'react'
-import { useNavigate, useSearchParams } from 'react-router-dom'
+import { Navigate, useNavigate, useSearchParams } from 'react-router-dom'
+import { match } from 'ts-pattern'
 
 import { authConfig } from '@/auth-config'
 import { POST } from '@/services/api/core/base-api'
 
+type AuthCallbackState =
+  | { tag: 'loading' }
+  | { tag: 'success' }
+  | { tag: 'error'; error: string }
+
 export function AuthCallback() {
   const navigate = useNavigate()
   const [searchParams] = useSearchParams()
-  const [error, setError] = React.useState<string | null>(null)
+  const [state, setState] = React.useState<AuthCallbackState>({
+    tag: 'loading',
+  })
 
   const code = searchParams.get('code')
+  const requested = React.useRef(false)
 
   React.useEffect(() => {
     if (!code) {
-      return setError('Ingen autentiseringskod hittades i URL:en.')
-    } else {
-      POST('/auth/callback', {
-        credentials: 'include',
-        body: {
-          code,
-          redirectUri: authConfig.redirectUri,
-        },
+      return setState({
+        tag: 'error',
+        error: 'Ingen autentiseringskod hittades i URL:en.',
       })
-        .then(() => navigate('/'))
-        .catch((err) => {
-          console.error(err)
-          setError('Ett fel uppstod vid autentisering.')
+    } else {
+      if (!requested.current) {
+        // Prevent duplicate requests caused by <StrictMode/> in development
+        requested.current = true
+        POST('/auth/callback', {
+          credentials: 'include',
+          body: {
+            code,
+            redirectUri: authConfig.redirectUri,
+          },
         })
+          .then(() => setState({ tag: 'success' }))
+          .catch((err) => {
+            console.error(err)
+            setState({
+              tag: 'error',
+              error: 'Ett fel uppstod vid autentisering.',
+            })
+          })
+      }
     }
   }, [code, navigate])
 
-  if (error) {
-    return (
+  return match(state)
+    .with({ tag: 'loading' }, () => (
+      <div className="flex items-center justify-center h-screen">
+        Autentiserar...
+      </div>
+    ))
+    .with({ tag: 'error' }, (e) => (
       <div className="flex flex-col items-center justify-center h-screen">
-        <div className="text-red-500 mb-4">{error}</div>
+        <div className="text-red-500 mb-4">{e.error}</div>
         <button
           onClick={() => (window.location.href = '/')}
           className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
@@ -41,12 +65,11 @@ export function AuthCallback() {
           Gå tillbaka
         </button>
       </div>
-    )
-  }
-
-  return (
-    <div className="flex items-center justify-center h-screen">
-      Autentiserar...
-    </div>
-  )
+    ))
+    .with({ tag: 'success' }, () => (
+      <div className="flex items-center justify-center h-screen">
+        <Navigate to="/" />
+      </div>
+    ))
+    .exhaustive()
 }
