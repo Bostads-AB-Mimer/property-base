31a217780947c4e3b81b3666cf092369cc6dbcd4
Andreas Lundqvist <momentiris@gmail.com>
2025-06-25 10:13:51 +0200

    base: apparently this is missing here

--- packages/property-base/src/adapters/property-adapter.ts
+++ packages/property-base/src/adapters/property-adapter.ts
@@ -38,6 +38,7 @@ const getPropertyById = async (
             energyReceived: true,
             energyIndex: true,
             heatingNature: true,
+            locationId: true,
           },
         },
       },
