2c8b2cffc5ddf4c9a7366a8c8886e36e281051ba
Henrik Persson <henrik.persson@iteam.com>
2025-07-01 16:28:48 +0200

    Add endpoint for getting maintenance units by property code

--- packages/property-base/src/types/maintenance-unit.ts
+++ packages/property-base/src/types/maintenance-unit.ts
@@ -2,12 +2,12 @@ import { z } from 'zod'
 
 export const MaintenanceUnitSchema = z.object({
   id: z.string(),
-  rentalPropertyId: z.string(),
+  rentalPropertyId: z.string().optional(),
   code: z.string(),
   caption: z.string(),
-  type: z.string().nullable(),
-  estateCode: z.string(),
-  estate: z.string(),
+  type: z.string().nullable().optional(),
+  propertyCode: z.string().nullable(),
+  propertyName: z.string().nullable(),
 })
 
 export type MaintenanceUnitInfo = z.infer<typeof MaintenanceUnitSchema>
